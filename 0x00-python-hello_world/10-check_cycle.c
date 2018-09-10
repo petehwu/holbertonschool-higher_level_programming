102e84f6ae5c2042c5d7038ccb0a72730208c7a5
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-09-10 15:02:44 -0700

    solution for 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks the list to see if there is circular link
+ * @list: linked likst to check
+ * Return: 0 if not circular 1 is circular
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *normal, *skipper;
+
+	if (!list)
+		return (0);
+	normal = list;
+	skipper = list;
+	do {
+		if ((skipper->next)->next)
+			skipper = (skipper->next)->next;
+		else
+			break;
+		if (normal == skipper)
+			return (1);
+		normal = normal->next;
+
+	} while (normal && skipper);
+	return (0);
+
+}
