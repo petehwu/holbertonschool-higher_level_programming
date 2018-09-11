6404aba367bd74549c5cddf1a6c4cadcc502f2d3
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-09-11 12:16:17 -0700

    fixed formatting issues

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,10 @@ if number < 0:
     lastdig *= -1
 if lastdig > 5:
     print("Last digit of {} is {} and is greater than 5"
-            .format(number, lastdig))
+          .format(number, lastdig))
 elif lastdig == 0:
     print("Last digit of {} is {} and is 0"
-            .format(number, lastdig))
+          .format(number, lastdig))
 else:
     print("Last digit of {} is {} and is less than 6 and not 0"
-            .format(number, lastdig))
-
+          .format(number, lastdig))
