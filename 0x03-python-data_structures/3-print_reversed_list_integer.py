6cee85d625db44f6b13159dc28ab5e81721bfe6c
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-09-18 14:56:10 -0700

    added code to handle when list is  Nonea

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if (len(my_list) == 0):
-        print("None")
-        return "None"
-    else:
+    if !(my_list is None):
         [print("{:d}".format(x)) for x in my_list[::-1]]
