5216f63f208dbd936f757319a46a8a504fb6c7c8
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-09-20 13:57:37 -0700

    adding from right side of string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,14 +5,15 @@ def roman_to_int(roman_string):
     try:
         roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
         arabic = [1, 5, 10, 50, 100, 500, 1000]
-        prev = arabic[roman.index(roman_string[0])]
+        prev = arabic[roman.index(roman_string[-1])]
         num = 0
         cur = 0
-        for c in roman_string:
+        for c in reversed(roman_string):
             cur = arabic[roman.index(c)]
-            num += cur
-            if (cur > prev):
-                num -= (prev * 2)
+            if (cur < prev):
+                num -= cur
+            else:
+                num += cur
             prev = cur
         return int(num)
     except ValueError:
