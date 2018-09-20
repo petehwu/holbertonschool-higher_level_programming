44446dad43034e837bdc3e2b53b8bba358cb3098
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-09-20 13:03:12 -0700

    fixed code style pep8 issues

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if roman_string is None:
         return int(0)
     try:
-        roman = ['I','V','X','L','C','D','M']
+        roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
         arabic = [1, 5, 10, 50, 100, 500, 1000]
         prev = arabic[roman.index(roman_string[0])]
         num = 0
@@ -12,10 +12,8 @@ def roman_to_int(roman_string):
             cur = arabic[roman.index(c)]
             num += cur
             if (cur > prev):
-                num -=(prev * 2)
+                num -= (prev * 2)
             prev = cur
         return int(num)
     except ValueError:
         return int(0)
-
-
