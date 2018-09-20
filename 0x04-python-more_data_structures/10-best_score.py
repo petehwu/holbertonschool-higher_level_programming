d7b01d05cefb93820927a9330aaaa81d6be22b4a
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-09-20 13:18:32 -0700

    added code to handle when dictionary is empty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or not a_dictionary:
         return None
     scores = list(a_dictionary.values())
     scores.sort(reverse=True)
