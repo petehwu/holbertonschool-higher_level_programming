5eb999fc7bd63f6b87910923bffa7784379a4500
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-09-21 11:20:14 -0700

    refactored code to be more pythonic

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,11 +2,4 @@
 def uniq_add(my_list=[]):
     if my_list is None:
         return None
-    my_list.sort()
-    prev = 0
-    tot = 0
-    for val in my_list:
-        if val != prev:
-            tot += val
-        prev = val
-    return tot
+    return sum(list(set(my_list)))
