9d9bb77baf78362a164a00c0dbcc13bdd152d882
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-10-16 22:01:49 -0700

    solution

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def append_after(filename="", search_string="", new_string= ""):
+    fileList = []
+
+    if not isinstance(filename, str):
+        raise TypeError("filename must be a string")
+    with open(filename, mode ="r", encoding="utf-8") as readFile:
+        for line in readFile:
+            fileList.append(line)
+            if search_string in line:
+                fileList.append(new_string)
+
+    with open(filename, mode="w", encoding="utf-8") as writeFile:
+        for l in fileList:
+            writeFile.write(str(l))
