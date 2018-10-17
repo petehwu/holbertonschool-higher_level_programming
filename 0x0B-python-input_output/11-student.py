aa24bb362e2b9c0336c3b812e82bfcbf0ae1e31f
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-10-16 21:43:18 -0700

    removed all checks

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -7,8 +7,6 @@ class Student():
     def __init__(self, first_name, last_name, age):
         """init method for Student class"""
 
-        if age < 0:
-            raise ValueError("age cannot be negative")
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
