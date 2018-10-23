524f28239bee4a7f23c0ac1b9ee836de25ab2c40
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-10-22 17:47:25 -0700

    changed evaluation for [] and None instead of Not

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -56,7 +56,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """convert list of dictionaries to string"""
-        if not list_dictionaries:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
