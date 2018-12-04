26eeac595ab10f2670d99368f7c83a0099bfb38d
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-12-04 13:38:52 -0800

    fixe capitalization

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- lists all shows with or without genre
-SELECT ts.title, tsg.genre_id FROM tv_shows ts left outer join tv_show_genres tsg On ts.id = tsg.show_id ORDER BY ts.title, tsg.genre_id ASC;
+SELECT ts.title, tsg.genre_id FROM tv_shows ts LEFT OUTER JOIN tv_show_genres tsg ON ts.id = tsg.show_id ORDER BY ts.title, tsg.genre_id ASC;
