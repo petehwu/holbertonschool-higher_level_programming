ad70a005222a1800fc889b098e634e9e72fae462
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-12-04 13:43:27 -0800

    fixed return result to show only id and name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- selects cities within california without using join
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE UPPER(name) = 'CALIFORNIA') ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE UPPER(name) = 'CALIFORNIA') ORDER BY cities.id ASC;
