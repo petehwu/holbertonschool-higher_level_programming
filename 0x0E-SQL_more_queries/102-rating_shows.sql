168f74fc0e5fecd788e9417d915c7dde1d712a27
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-12-04 15:53:57 -0800

    fix displayed column name

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- lists shows by rating
-SELECT a.title, SUM(b.rate) FROM tv_shows a, tv_show_ratings b
+SELECT a.title, SUM(b.rate) AS rating FROM tv_shows a, tv_show_ratings b
 WHERE a.id = b.show_id 
 GROUP BY b.show_id
 ORDER BY 2 DESC;
