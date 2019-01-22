2255c85fb7322df41c37fc6160fae68182f4bb8a
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-21 18:23:10 -0800

    change from dictionary key to get

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,10 @@ import sys
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
                                                               sys.argv[2])
-    res = requests.get(url)
+    ds = {"since":"since=2019-01-18T00:00:00Z"}
+    header = {"Accept": "application/vnd.github.v3+json"}
+    res = requests.get(url, headers=header, params=ds)
     res_dict = res.json()
     for d in res_dict[:10]:
-        print("{}: {}".format(d.get("sha"), d["commit"]["author"]["name"]))
+        print("{}: {}".format(d.get("sha"), 
+                              d.get("commit").get("author").get("name")))
