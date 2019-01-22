bb12b07f34b3a7e2dd36e5bc8427a26c509e841a
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-21 18:35:17 -0800

    reversed owner and repo

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,12 +4,12 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1],
-                                                              sys.argv[2])
-    ds = {"since":"since=2019-01-18T00:00:00Z"}
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
+                                                              sys.argv[1])
+    ds = {"since": "since=2019-01-18T00:00:00Z"}
     header = {"Accept": "application/vnd.github.v3+json"}
     res = requests.get(url, headers=header, params=ds)
     res_dict = res.json()
     for d in res_dict[:10]:
-        print("{}: {}".format(d.get("sha"), 
+        print("{}: {}".format(d.get("sha"),
                               d.get("commit").get("author").get("name")))
