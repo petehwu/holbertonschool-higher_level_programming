298c6e52904d087ebe3b1dff79fa3c3c8969e2b0
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-21 16:14:59 -0800

    used header.get instead of dictionary key lookup

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This script fetches a URL"""
+"""This script etches a URL"""
 import requests
 import sys
 
 if __name__ == "__main__":
     html = requests.get(sys.argv[1])
-    print("{}".format(html.headers['X-Request-Id']))
+    print("{}".format(html.headers.get('X-Request-Id')))
