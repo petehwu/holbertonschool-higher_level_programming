bdc381eaf9ee98e6d814d57c9a140623ce02158c
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-21 21:32:24 -0800

    101

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This script will do a lookup on the starwars lookup using requests
+package"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://swapi.co/api/people/"
+    if len(sys.argv) == 1:
+        val = ""
+    else:
+        val = sys.argv[1]
+    res = requests.get(url, params={"search": val})
+    tot = res.json().get('count')
+    res_dict = res.json()
+    added = 0
+    page = 1
+    print("Number of results: {}".format(tot))
+    while added < tot:
+        for i in res_dict['results']:
+            added += 1
+            print(i['name'])
+        page += 1
+        res = requests.get(url, params={"search": val, "page": page})
+        res_dict = res.json()
