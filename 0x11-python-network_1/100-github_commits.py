71c8d5d3b007617d44a8c74fea21039a6b32b127
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-01-21 20:36:07 -0800

    not adding commmits with different dates

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,10 +6,19 @@ import sys
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
                                                               sys.argv[1])
-    ds = {"since": "since=2019-01-18T00:00:00Z"}
     header = {"Accept": "application/vnd.github.v3+json"}
-    res = requests.get(url, headers=header, params=ds)
+    res = requests.get(url, headers=header)
     res_dict = res.json()
-    for d in res_dict[:10]:
-        print("{}: {}".format(d.get("sha"),
-                              d.get("commit").get("author").get("name")))
+    ele = []
+    num = 0
+    runner = 0
+    for d in res_dict:
+        if d.get("commit").get("author").get("date") ==\
+           d.get("commit").get("committer").get("date"):
+            ele.append(d)
+            num += 1
+        if num == 10:
+            break
+    for x in ele:
+        print("{}: {}".format(x.get("sha"),
+                              x.get("commit").get("author").get("name")))
