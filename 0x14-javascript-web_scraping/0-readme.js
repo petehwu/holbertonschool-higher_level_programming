6245f27c9b2cc879d83d77c169dc796aa3a7abfa
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-02-19 12:25:35 -0800

    fixed semistandard issue and removed extra file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // reads and prints content of a file
 let file = process.argv[2];
+let fs = require('fs');
 if (file === undefined) {
   console.log('Error: Usage ./0-readme.js <file_to_read>');
 } else {
-  let fs = require('fs');
-  fs.readFile(file, 'utf-8', function(err, data) {
+  fs.readFile(file, 'utf-8', function (err, data) {
     if (err) {
       console.log(err);
     } else {
