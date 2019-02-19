521c38f0f8ccd0e057361351e5953c1f508f70a1
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2019-02-19 13:28:00 -0800

    use test

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,20 +4,21 @@
 const request = require('request');
 const uri = process.argv[2];
 let count = 0;
+let search = new RegExp('18/');
 if (uri === undefined) {
-  console.log('Error: Usage ./4-starwars_count.js <film_id_number>');
+  console.log('Error: Usage ./4-starwars_count.js <starwars_api_url>');
 } else {
   request.get({ url: uri, json: true }, function (error, response, body) {
-    if (error) {
-      console.log(error);
-    } else if (response.statusCode !== 200) {
-      console.log(response.statusCode);
+    if (error || response.statusCode !== 200) {
+      console.log('0');
     } else {
-      let res = body.results;
       let index = 0;
-      for (index = 0; index < res.length; index++) {
-        if (res[index].characters.includes('https://swapi.co/api/people/18/')) {
-          count++;
+      let index2 = 0;
+      for (index = 0; index < body.results.length; index++) {
+        for (index2 = 0; index2 < body.results[index].characters.length; index2++) {
+          if (search.test(body.results[index].characters[index2])) {
+            count++;
+          }
         }
       }
       console.log(count.toString());
